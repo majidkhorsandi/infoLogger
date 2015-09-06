b705f22e7cea33d404c05f893b061e3401969f80
Majid Khorsandi <majid@Majids-MacBook-Pro.local>
2015-09-07 00:51:23 +0200

    Ping the server address provided by user to make sure its a valid reachable address before monitor is started. Make the code more self-defending

--- monitor.js
+++ monitor.js
@@ -2,13 +2,16 @@
  * Created by majkho on 2015-09-02.
  */
 var DEFAULT_PORT = 3000;
+var DEFAULT_INTERVAL = 3;
 var DEFAULT_SERVER = 'localhost';
 var STATUS_OK = 200;
+var SEC_TO_MILLISEC = 1000;
 
 var monitor = require("os-monitor");
 var os = require('os');
 var usage = require('usage');
 var Client = require('node-rest-client').Client;
+var ping = require('ping');
 var client = new Client();
 var serverAddress;
 var	monitorInterval;
@@ -21,26 +24,34 @@ isServerAlive(startMonitor);
 
 
 function isServerAlive(startMonitor){
-console.log('Pinging the remote server before starting to monitor...')
+	console.log('Pinging the remote server before starting to monitor...');
+	ping.sys.probe(serverAddress, function(isAlive){
+            if (!isAlive) {
+            	console.log('Monitor server is not reachable. Please check the address.');
+            	process.exit(1);
+            }
+    });
 	client.get('http://' + serverAddress + ':' + DEFAULT_PORT + '/ping', function(data, response) {
 		if (response.statusCode != STATUS_OK) {
-			console.log('Could not ping the monitor server. Please check if server is started');
+			console.log('Could not talk to the monitor service. Please check if service is started.');
+			process.exit(1);
 		} else {
-			console.log(response.body + 'Monitor server is up and running...');
+			console.log('Monitor server is up and running...');
 			startMonitor();
 		}
 	}).on('error', function(err) {
-		console.log('Could not ping the monitor server. Please check if server is started');
+		console.log('Something went wrong when trying to talk to monitor service');
 	});
 };
 
 function startMonitor(){
-console.log('Monitor is started. Stop it by pressing Ctrl+c');
-monitor.start({ delay: monitorInterval });
-monitor.on('monitor', function(event) {
+	console.log('Monitor is started. Stop it by pressing Ctrl+c');
+	monitor.start({ delay: monitorInterval*SEC_TO_MILLISEC });
+	monitor.on('monitor', function(event) {
 	usage.lookup(processID, function(err, result) {
 		if (!result) {
-			return;
+			console.log('Process ID could not be found');
+			process.exit(1)
 		}
 		if (err) {
 			throw err;
@@ -70,7 +81,17 @@ function generateUsgeJsonMessage(result) {
 
 function assignCommandLineArguments() {
 	processID = process.argv[2];
-    monitorInterval = process.argv[4] == null ? DEFAULT_PORT : process.argv[4];
+	if (process.argv[4] == null) {
+		monitorInterval = DEFAULT_INTERVAL;
+	} else if (process.argv[4] < 1) {
+		monitorInterval = 1;
+		console.log('Min interval can not be less than one second. Changing interval to 1 sec');
+	} else if (process.argv[4] > 60) {
+		monitorInterval = 60;
+		console.log('Max interval can not be more than one minute. Changing interval to 1 min');
+	} else {
+		monitorInterval = process.argv[4];
+	}
     serverAddress = process.argv[3] == null ? DEFAULT_SERVER : process.argv[3]
 }
 
@@ -95,6 +116,6 @@ function printUsage() {
 	console.log(':> node monitor.js <pid> <server-address> <interval>')
 	console.log('server-address = IP address of server that collects the information. Default is localhost')
 	console.log("pid = ID of the process you want to monitor")
-	console.log('interval = Data collection interval in ms (optional)')
+	console.log('interval = Data collection interval in seconds (optional)')
 	console.log('********************************************')
 }
