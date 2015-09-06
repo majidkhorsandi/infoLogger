98e7f93e799f827f7c1babc45919a5979e4e78f8
Majid Khorsandi <majid@Majids-MacBook-Pro.local>
2015-09-06 19:28:25 +0200

    Monitor app pings the server before it starts posting to it to make sure server is listening

--- monitor.js
+++ monitor.js
@@ -1,67 +1,101 @@
 /**
  * Created by majkho on 2015-09-02.
  */
+var DEFAULT_PORT = 3000;
+var DEFAULT_SERVER = 'localhost';
+var STATUS_OK = 200;
 
-var monitor = require("os-monitor")
-	, os = require('os')
-	, usage = require('usage');
-	var Client = require('node-rest-client').Client;
-
-    var client = new Client();
-
-if (process.argv[2] == null) {
-	printUsage();
-	return;
-};
-var processID = process.argv[2];
-var monitorInterval = process.argv[3] == null ? 3000 : process.argv[3];
-//var args;
+var monitor = require("os-monitor");
+var os = require('os');
+var usage = require('usage');
+var Client = require('node-rest-client').Client;
+var ping = require('net-ping');
+var client = new Client();
+var serverAddress;
+var	monitorInterval;
+var	processID;
 
 
-monitor.start({ delay: monitorInterval // interval in ms between monitor cycles
-	, freemem: 1000000000 // freemem under which event 'freemem' is triggered
-	, uptime: 1000000 // number of secs over which event 'uptime' is triggered
-	, critical1: 0.7 // loadavg1 over which event 'loadavg1' is triggered
-	, critical5: 0.7 // loadavg5 over which event 'loadavg5' is triggered
-	, critical15: 0.7 // loadavg15 over which event 'loadavg15' is triggered
-	, silent: false // set true to mute event 'monitor'
-	, stream: false // set true to enable the monitor as a Readable Stream
-	, immediate: false // set true to execute a monitor cycle at start()
-});
+if (!isCorrectUsage()) {return};
+assignCommandLineArguments();
+isServerAlive(startMonitor);
 
-//console.log('Operating system: ' + os.type());
 
-monitor.on('monitor', function(event) {
-	console.log(event.type, 'Collecting information every ' + monitorInterval + ' second');
-	usage.lookup(processID, function(err, result) {
-		if (result) {
-			console.log(result);
-			console.log(result.cpu);
+function isServerAlive(startMonitor){
+console.log('Pinging the remote server before starting to monitor...')
+	client.get('http://' + serverAddress + ':' + DEFAULT_PORT + '/ping', function(data, response) {
+		if (response.statusCode != STATUS_OK) {
+			console.log('Could not ping the monitor server. Please check if server is started');
 		} else {
-			return
+			console.log(response.body + 'Monitor server is up and running...');
+			startMonitor();
 		}
-		var currentDate = new Date();
-		var currentTime = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
-		var args = {
-          data: { 	time: currentTime,
-			  		os: os.type(),
-			  		process_cpu_usage: result.cpu,
-		  			process_mem_usage: result.memory,
-		  			total_mem: os.totalmem(),
-		  			total_free_mem: os.freemem()},
-          headers:{"Content-Type": "application/json"}
-        };
+	}).on('error', function(err) {
+		console.log('Could not ping the monitor server. Please check if server is started');
+	});
+};
 
-		client.post("http://localhost:3000/stat", args, function(data,response) {
-
+function startMonitor(){
+console.log('Monitor is started. Stop it by pressing Ctrl+c');
+monitor.start({ delay: monitorInterval });
+monitor.on('monitor', function(event) {
+	usage.lookup(processID, function(err, result) {
+		if (!result) {
+			return;
+		}
+		if (err) {
+			throw err;
+		}
+		client.post("http://" + serverAddress + ":" + DEFAULT_PORT + "/stat", generateUsgeJsonMessage(result), function(data,response) {
+			console.log('Sending usage statistics to the monitor server')
+        }).on('error', function(err){
+        	console.log('something went wrong with connecting to the server!');
+        	monitor.stop();
+        	return;
         });
 	})
 });
+};
+
+function generateUsgeJsonMessage(result) {
+	return {
+    	data: {time: getCurrentTime(),
+        os: os.platform() + '-' + os.release(),
+        process_cpu_usage: result.cpu,
+        process_mem_usage: result.memory,
+        total_mem: os.totalmem(),
+        total_free_mem: os.freemem()},
+    	headers:{"Content-Type": "application/json"}
+    };
+}
+
+function assignCommandLineArguments() {
+	processID = process.argv[2];
+    monitorInterval = process.argv[4] == null ? DEFAULT_PORT : process.argv[4];
+    serverAddress = process.argv[3] == null ? DEFAULT_SERVER : process.argv[3]
+}
+
+function getCurrentTime() {
+	var currentDate = new Date();
+	var currentHour = currentDate.getHours() < 10 ? '0' + currentDate.getHours() : currentDate.getHours();
+	var currentMinute = currentDate.getMinutes() < 10 ? '0' + currentDate.getMinutes() : currentDate.getMinutes();
+	var currentSecond = currentDate.getSeconds() < 10 ? '0' + currentDate.getSeconds() : currentDate.getSeconds();
+	var currentTime = currentHour + ":" + currentMinute + ":" + currentSecond;
+	return currentTime;
+}
+
+function isCorrectUsage() {
+	if (process.argv[2] == null) {
+		printUsage();
+		return false;
+	} else return true;
+}
 
 function printUsage() {
 	console.log('\n********** Correct Usage ******************')
-	console.log('node monitor.js pid interval')
-	console.log("pid = spotify's process id")
-	console.log('interval = data collection interval in ms (optional)')
+	console.log(':> node monitor.js <pid> <server-address> <interval>')
+	console.log('server-address = IP address of server that collects the information. Default is localhost')
+	console.log("pid = ID of the process you want to monitor")
+	console.log('interval = Data collection interval in ms (optional)')
 	console.log('********************************************')
 }
--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 var express = require('express')
 	, http = require('http')
 	, path = require('path')
+	, winston = require('winston')
 	, bodyParser = require('body-parser')
 	, fs = require('fs');
 
@@ -19,19 +20,31 @@ app.use(bodyParser());
 
 fs.exists(createLogFileName(), function(exists) {
 	if (!exists) {
-		console.log('file does not exist . I should create it');
-		var fields = ['time    ','os    ','process_cpu_usge    ','process_mem_usage    ','total_mem    ', 'total_free_mem    \n'];
+		winston.info('No log file for today was found. The log file will be created.');
+		var fields = ['time    ',
+						'os    ',
+						'process_cpu_usge    ',
+						'process_mem_usage    ',
+						'total_mem    ',
+						'total_free_mem    \n'];
     	fs.writeFile(createLogFileName(),fields, function(err){
     		if (err) throw err;
 		})
 	}
 })
 
+app.get('/ping', function(req, res){
+	res.statusCode = 200;
+	return res.send('pong!');
+});
+
+// server receives a new post request at uri /stat. If everything fine, writes new entry to log file
 app.post('/stat', function(req, res) {
 	if(!req.body.hasOwnProperty('os') || !req.body.hasOwnProperty('process_cpu_usage')) {
     		res.statusCode = 400;
     		return res.send('Error 400: Post syntax incorrect.');
   	}
+  	winston.info('A new log entry received: ');
 	var newStat = {
 		time: req.body.time,
     	os: req.body.os,
@@ -40,21 +53,19 @@ app.post('/stat', function(req, res) {
 		total_mem : req.body.total_mem,
 		total_free_mem : req.body.total_free_mem
 	};
-
-    fs.appendFile(createLogFileName(), createNewLine(req), function (err) {
+	winston.info(newStat);
+    fs.appendFile(createLogFileName(), createNewLogEntry(req), function (err) {
       	if (err) {
-      		console.log(err);
+      		winston.info(err);
       		throw err;
       	}
     })
-
-	console.log(statistics);
   	res.json(true);
 });
 
-
+//server is created and started
 http.createServer(app).listen(app.get('port'), function() {
-	console.log('Express server listening on port ' + app.get('port'));
+	winston.info('Log server listening on port ' + app.get('port'));
 });
 
 
@@ -66,7 +77,7 @@ function createLogFileName() {
     return '/tmp/log' + yy + mm + dd + '.txt';
 }
 
-function createNewLine(req) {
+function createNewLogEntry(req) {
 	var newLine= req.body.time + '    ' + req.body.os + '    ' + req.body.process_mem_usage + '    ';
 		newLine = newLine + req.body.process_cpu_usage + '    ' + req.body.total_mem + '    ' + req.body.total_free_mem + '    \n';
 		return newLine;
